d1e1e7ee3bbb7b782fc3ea9e525027184075f0e5
Rehan-Khan-Engineer <rk116159@gmail.com>
2022-10-02 21:23:03 +0530

    updated data source

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
@@ -8,7 +8,11 @@ function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<AllMeetupsPage></AllMeetupsPage>} />
+        <Route
+          path="/Meetups-App-React"
+          element={<AllMeetupsPage></AllMeetupsPage>}
+        />
+        <Route path="/" element={<Navigate to="/Meetups-App-React" />} />
         <Route path="/new-meetup" element={<NewMeetupPage></NewMeetupPage>} />
         <Route path="/favorite" element={<FavoritesPage></FavoritesPage>} />
       </Routes>
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,65 +1,65 @@
 import MeetupList from "../components/meetups/MeetupList";
 import { useState, useEffect } from "react";
 
-// const DUMMY_DATA = [
-//   {
-//     id: "m1",
-//     title: "This is a first meetup",
-//     image:
-//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-//     address: "Meetupstreet 5, 12345 Meetup City",
-//     description:
-//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-//   },
-//   {
-//     id: "m2",
-//     title: "This is a second meetup",
-//     image:
-//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-//     address: "Meetupstreet 5, 12345 Meetup City",
-//     description:
-//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-//   },
-// ];
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+];
 
 function AllMeetupsPage() {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch("https://meetups-app-8c4ad-default-rtdb.firebaseio.com/meetups.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const helperArray = [];
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch("https://meetups-app-8c4ad-default-rtdb.firebaseio.com/meetups.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const helperArray = [];
 
-        for (const key in data) {
-          const element = {
-            id: key,
-            ...data[key],
-          };
+  //       for (const key in data) {
+  //         const element = {
+  //           id: key,
+  //           ...data[key],
+  //         };
 
-          helperArray.push(element);
-        }
+  //         helperArray.push(element);
+  //       }
 
-        setIsLoading(false);
-        setLoadedMeetups(helperArray);
-      });
-  }, []);
+  //       setIsLoading(false);
+  //       setLoadedMeetups(helperArray);
+  //     });
+  // }, []);
 
-  if (isLoading) {
-    return (
-      <section>
-        <p>Loading...</p>
-      </section>
-    );
-  }
+  // if (isLoading) {
+  //   return (
+  //     <section>
+  //       <p>Loading...</p>
+  //     </section>
+  //   );
+  // }
 
   return (
     <section>
       <h1>All Meetups page</h1>
-      <MeetupList meetups={loadedMeetups} />
-      {/* <MeetupList meetups={DUMMY_DATA} /> */}
+      {/* <MeetupList meetups={loadedMeetups} /> */}
+      <MeetupList meetups={DUMMY_DATA} />
     </section>
   );
 }
